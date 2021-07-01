6ead1bdc5f6f1d3e2eea42127bfd38eb4b573e10
CorsairRGB 16gb DDR4 3200MHz RAM <69950985+insomnia-creator@users.noreply.github.com>
2021-07-01 11:59:10 +0530

    Did I mention that I need to fix this thing???

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
 const Discord = require('discord.js');
 //require the discord.js module
 const client = new Discord.Client({
-    intents: Discord.Intents.NON_PRIVILEGED,
+    intents: Discord.Intents.ALL,
     allowedMentions: {
         repliedUser: false
     },
