f8e58c6d8873a6983eedd16500a3d4054bedf544
CorsairRGB 16gb DDR4 3200MHz RAM <69950985+insomnia-creator@users.noreply.github.com>
2021-07-26 12:30:39 +0530

    And fixed an error

--- src/commands/ban.js
+++ src/commands/ban.js
@@ -25,7 +25,7 @@ module.exports = {
         //we don't want to you know, d e l a y
         //omg i copy from kickkk???? omgm omgmgmgmgmgmgmgmgmgmgmggmgm among ussssss!!!!!!
 
-        memberObject.ban(reason ? reason.value : null).catch(async e => {
+        memberObject.ban().catch(async e => {
             await interaction.reply({
                 content: "There was an error. This might be because that user is higher up on the roles list or has better permissions.",
                 ephemeral: true
